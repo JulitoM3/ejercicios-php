2a7e1f858dc1296ec32b28bbd1ad7e94a25ae263
Julio Guerrero <juliocaramelo44@gmail.com>
2025-04-12 12:32:19 -0600

    Nuevos ejercicios

--- ejercicios_poo.php
+++ ejercicios_poo.php
@@ -36,3 +36,31 @@
 * ("Guau" y "Miau", respectivamente). 
 
 */
+
+
+/*
+ * Ejercicio 3: Interfaces 
+ */
+
+/*
+
+* Crea una interfaz llamada Empleado con un método calcularPago().
+* Implementa esa interfaz en las clases EmpleadoTiempoCompleto y EmpleadoPorHora, con lógica distinta para el cálculo del pago.
+* Crea un array con distintos empleados y muestra su pago usando un foreach. 
+
+*/
+
+
+/*
+ * Ejercicio 4: Clases Abstractas
+ */
+/*
+* Crea una clase abstracta llama da Vehiculo que tenga:
+* -Propiedades: marca, modelo y año.
+* - Metodo abstracto: calcularVelocidadMaxima(),
+* - Metodo concreto: mostrarDetalles() que imprima los detalles del vehículo.
+* Luego, crea dos clases hijas: Coche y Moto que hereden de Vehiculo y
+* implementen el método calcularVelocidadMaxima() con lógica específica para cada tipo de vehículo.
+* usando Arrays crea varias instancias con diferentes modelos y muestra sus detalles y velocidad máxima 
+
+*/
