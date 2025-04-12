bb6c584c89bdd9889b5fbd33e123e9c79cf40b3e
Julio Guerrero <juliocaramelo44@gmail.com>
2025-04-11 23:27:01 -0600

    ejercicios

---
+++ ejercicios_poo.php
@@ -0,0 +1,38 @@
+<?php
+
+
+/*
+ * Ejercicio 1: Clase Básica y Encapsulación
+ */
+
+/*
+ * Crea una clase llamada Libro que contenga las siguientes propiedades:
+ * -Título (string)
+ * -Autor (string)
+ * -Número de páginas (entero)
+ ***/
+
+/*
+ * Implementa:
+ * Un constructor para inicializar las propiedades.
+ * Métodos getters y setters para cada propiedad.
+ * Un método mostrarInformacion() que imprima todos los detalles del libro.
+ */
+
+
+
+
+/*
+ * Ejercicio 2: Herencia y Polimorfismo    
+ */
+
+/*
+
+* Crea una clase base llamada Animal que tenga:
+* Una propiedad nombre.
+* Un método hacerSonido() que imprima un mensaje genérico.
+* Luego, crea dos clases hijas: Perro y Gato que hereden de Animal y 
+* sobrescriban el método hacerSonido() para mostrar sonidos específicos 
+* ("Guau" y "Miau", respectivamente). 
+
+*/
